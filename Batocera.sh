95539b96696da440173b00900c083ff88eda8131
Jhaiywroumne <jhaiywroumne@gmail.com>
2024-04-28 03:06:33 +0800

    Update Batocera.sh

--- Batocera.sh
+++ Batocera.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
 ARCH=$(uname -m)
-case "$ARCH" in
-  x86_64|amd64)
-    echo X64;;
-  i?86)
-    echo X32;;
-  arm*)
-    echo ARM;;
-  powerpc|ppc64)
-    echo PowerPC;;
-  *)
 # Download Zerotier
 if [ "$ARCH" == x86_64* ]; then
  curl -LJO https://github.com/Jhaiywroumne/BatoZero/releases/download/Latest/zerotier-one-aarch64.tar.gz
@@ -41,3 +31,4 @@ rm zerotier-one-aarch64.tar.gz zerotier-one-x86_64.tar.gz
 # Setup Startup File
 cd /userdata/system/services
 curl -LJO https://github.com/Jhaiywroumne/BatoZero/blob/main/Zerotier
+;;
