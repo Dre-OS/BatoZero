93b52fc65a72b02bd80b2602f08bd53e1dc1ae4d
Jhaiywroumne <jhaiywroumne@gmail.com>
2024-04-28 02:48:52 +0800

    Update Batocera.sh

--- Batocera.sh
+++ Batocera.sh
@@ -4,9 +4,9 @@ ARCH=$(uname -m)
 
 # Download Zerotier
 if [[ "$ARCH" == x86_64* ]]; then
-    curl -LJO https://github.com/Jhaiywroumne/BatoZero/releases/download/Latest/zerotier-one-aarch64.tar.gz
+curl -LJO https://github.com/Jhaiywroumne/BatoZero/releases/download/Latest/zerotier-one-aarch64.tar.gz
 elif [[ "$ARCH" == arm* ]]; then
-    curl -LJO https://github.com/Jhaiywroumne/BatoZero/releases/download/Latest/zerotier-one-x86_64.tar.gz
+curl -LJO https://github.com/Jhaiywroumne/BatoZero/releases/download/Latest/zerotier-one-x86_64.tar.gz
 else
     echo Unsupported system architecture
     exit 1 # terminate and indicate error
